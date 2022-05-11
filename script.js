34121f5081b8c87c5924ea906f7cead464115bb2
Grant Stahr <76236444+InsulatorGMan@users.noreply.github.com>
2022-05-11 07:51:21 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+function arsenal(){
+    var content = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/HonestlyDex/DexHub/main/V3.lua"))()';
+
+
+    navigator.clipboard.writeText(content)
+        .then(() => {
+        console.log("Text copied to clipboard...")
+    })
+        .catch(err => {
+        console.log('Something went wrong', err);
+    })
+ 
+}
+function bss(){
+    var content = "loadstring(game:HttpGet(('https://raw.githubusercontent.com/AltsegoD/script/master/BeeSwarmSimulator.lua')))()";
+
+
+    navigator.clipboard.writeText(content)
+        .then(() => {
+        console.log("Text copied to clipboard...")
+    })
+        .catch(err => {
+        console.log('Something went wrong', err);
+    })
+ 
+}
+function cmdx(){
+    var content = "loadstring(game:HttpGet(('https://raw.githubusercontent.com/AltsegoD/script/master/BeeSwarmSimulator.lua')))()";
+
+
+    navigator.clipboard.writeText(content)
+        .then(() => {
+        console.log("Text copied to clipboard...")
+    })
+        .catch(err => {
+        console.log('Something went wrong', err);
+    })
+ 
+}
